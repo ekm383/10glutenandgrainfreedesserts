6f494e406a6d6816861ef760da864c75c4f52b13
Eric Manongdo <eric@superscriptcreative.com>
2020-10-27 12:45:33 -1000

    Inject mailchimp in Head

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,13 @@
 const React = require("react")
 
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
     <script
       id="mcjs"
       type="text/javascript"
       dangerouslySetInnerHTML={{
         __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/20bb22ff7fdb4f70b68522c88/dd1f35af4890c63fda8645cf6.js");`,
       }}
-    />, //Injecting SalesIq Script on body of the page
+    />, //Injecting mailchimp popup in Head
   ])
 }

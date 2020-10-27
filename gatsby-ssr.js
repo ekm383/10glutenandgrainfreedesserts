532f9f78a0206aa065b877193422d812c80862c9
Eric Manongdo <eric@superscriptcreative.com>
2020-10-27 12:05:15 -1000

    Mailchimp script added in SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,16 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+import React from "react"
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script id="mcjs">
+      !function(c,h,i,m,p)
+      {
+        ((m = c.createElement(h)),
+        (p = c.getElementsByTagName(h)[0]),
+        (m.async = 1),
+        (m.src = i),
+        p.parentNode.insertBefore(m, p))
+      }
+      (document,"script","https://chimpstatic.com/mcjs-connected/js/users/20bb22ff7fdb4f70b68522c88/dd1f35af4890c63fda8645cf6.js");
+    </script>,
+  ])
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
 import "./layout.css"
 import HeaderLogo from "./globals/header/HeaderLogo"
 import Footer from "./globals/footer/Footer"
@@ -14,19 +13,6 @@ if (typeof window !== "undefined") {
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
-      <Helmet>
-        <script id="mcjs">
-          !function(c,h,i,m,p)
-          {
-            ((m = c.createElement(h)),
-            (p = c.getElementsByTagName(h)[0]),
-            (m.async = 1),
-            (m.src = i),
-            p.parentNode.insertBefore(m, p))
-          }
-          (document,"script","https://chimpstatic.com/mcjs-connected/js/users/20bb22ff7fdb4f70b68522c88/dd1f35af4890c63fda8645cf6.js");
-        </script>
-      </Helmet>
       <HeaderLogo />
       {children}
       <Footer />
